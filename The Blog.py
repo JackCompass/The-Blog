a8269fbb229056173dba50d20212340427bdca85
Jack <kumaranuj1303@gmail.com>
2020-08-16 22:39:14 +0530

    Connected contacts page to the database

--- The Blog.py
+++ The Blog.py
@@ -1,7 +1,20 @@
-from flask import Flask,render_template
- 
+from flask import Flask,render_template,request
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+import  pymysql
+
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/thetravelers'
+db = SQLAlchemy(app)
+
+class Contacts(db.Model):
+	sno = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(30), nullable=False)
+	email = db.Column(db.String(30), nullable=False)
+	subject = db.Column(db.String(50), nullable=False)
+	message = db.Column(db.String(500), nullable=False)
+	time = db.Column(db.String(20), nullable=False)
 @app.route("/")
 def main():
 	return render_template('index.html')
@@ -18,8 +31,19 @@ def articles():
 def about():
 	return render_template('about.html')
 
-@app.route("/contact")
+@app.route("/contact", methods = ['GET', 'POST'])
 def contact():
+	if request.method == 'POST':
+		''' User will enter data and it will put it in database'''
+		name = request.form.get('name')
+		email = request.form.get('email')
+		subject = request.form.get('subject')
+		message = request.form.get('message')
+
+		query = Contacts(name = name, email = email, subject = subject, message = message, time = datetime.now())
+		db.session.add(query)
+		db.session.commit()
+
 	return render_template('contact.html')
 
 app.run(debug=True)
